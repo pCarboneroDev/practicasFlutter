82e0f71effdf36229b92925bd078b6bb54b1e53a
pCarboneroDev <pcarboneroalmellones@gmail.com>
2025-03-17 14:38:51 +0100

    cambio de user

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      //home: HomeScreen()
-      home: CounterScreen()
+      home: HomeScreen()
+      //home: CounterScreen()
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'package:practicas_flutter/screens/counter_screen.dart';
 
 class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    const fontSize35 = const TextStyle(fontSize: 35);
-    int clicks = 0;
 
     return Scaffold(
       appBar: AppBar(
@@ -15,22 +14,32 @@ class HomeScreen extends StatelessWidget{
         title: const Text('HOME SCREEN', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),), 
         elevation: 0.0,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget> [
-            const Text('Contador de clicks:', style: fontSize35),
-            Text('$clicks', style: TextStyle(fontSize: 45))
-          ],
-        ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Column(
+        spacing: 500,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.blue,
+            child: const Icon(Icons.add_circle, color: Colors.white,),
+            onPressed: () 
+            { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CounterScreen()),
+            ); }
+            ),
+
+            FloatingActionButton(
+            backgroundColor: Colors.blue,
+            child: const Icon(Icons.add_circle, color: Colors.white,),
+            onPressed: () 
+            { Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CounterScreen()),
+            ); }
+            ),
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.add, color: Colors.white,),
-        onPressed: () { clicks++; print(clicks); }
-        ),
     );
   }
 
